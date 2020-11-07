78ef279ac572268a245cd96d9977b6ae39f6f43c
Rxm0520 <67207414+Rxm0520@users.noreply.github.com>
2020-11-07 17:47:32 -0500

    Add files via upload

--- Project2/index.php
+++ Project2/index.php
@@ -1,34 +1,43 @@
+<?php
+session_save_path('/home/sli34/public_html/Project2/session/');
+session_start();
+?>
+
 <!DOCTYPE html>
-<html>
-<body>
+    <html>
+        <head>
+            <link rel="stylesheet" href="./style.css"/> 
+        </head>
 
-<?php include("top.html"); ?>
+        <body>
+            <?php include("top.html"); ?>
 
-<form action="questions.php" method="post">
-<fieldset>
-<ul>
-<li>
-<strong>Name:</strong>
-<input type="text" name="name" size="16" required/>
-</li>
+            <form action="questions.php" method="post">
+                <fieldset>
+                    <ul>
+                        <li>
+                            <strong>Name:</strong>
+                            <input type="text" name="name" size="16" />
+                        </li>
 
-<li><strong>Gender:</strong>
-<label><input type="radio" name="gender" value="Male" checked="checked" />Male</label>
-<label><input type="radio" name="gender" value="Female" />Female</label>
-</li>
+                        <li>
+                            <strong>Gender:</strong>
+                            <label><input type="radio" name="gender" value="Male" checked="checked" />Male</label>
+                            <label><input type="radio" name="gender" value="Female" />Female</label>
+                        </li>
 
-<li>
-<strong>Age:</strong>
-<input type="text" name="age" size="6" maxlength="2" required/>
-</li>
-</ul>
+                        <li>
+                            <strong>Age:</strong>
+                            <input type="text" name="age" size="6" maxlength="2" />
+                        </li>
+                    </ul>
 
-<input type="submit" value="Next">
-</fieldset>
-</form>
+                    <input type="submit" value="Next"> <a href="signin.php">Already Have an Account?</a>
+                </fieldset>
+            </form>
+        </body>
+
+    </html>
+
 
 <?php include("bottom.html"); ?>
-
-
-</body>
-</html>
